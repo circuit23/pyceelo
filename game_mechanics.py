bc79d37553698b1dbae5ace16935335436abf2f8
circuit23 <circuit.23@gmail.com>
2023-09-11 21:02:27 -0400

    Remove print statements from game_mechanics.py
    Removed in-line print statements from the game_mechanics.py script for cleaner code and efficient game process. The game result is now directly returned, thereby eliminating unnecessary console prints. Changes also reflect in main.py, where the player's result is explicitly printed. This change confirm the game's functionality while maintaining a neat console output.

---
+++ game_mechanics.py
@@ -0,0 +1,43 @@
+from random import randint
+
+
+def get_roll(amount=3):
+    return [randint(1, 6) for _ in range(amount)]
+
+
+def get_result(roll_list):
+    roll_list.sort()
+    roll_1, roll_2, roll_3 = roll_list
+
+    if (roll_1, roll_2, roll_3) == (4, 5, 6):
+        return "WW", "WIN: 4-5-6 straight kill"
+    elif all(item == roll_list[0] for item in roll_list):
+        return f"T{roll_1}", f"TRIPS: {roll_1}"
+    elif roll_1 == roll_2:
+        return f"P{roll_3}", f"POINT: {roll_3}"
+    elif roll_2 == roll_3:
+        return f"P{roll_1}", f"POINT: {roll_1}"
+    elif (roll_1, roll_2, roll_3) == (1, 2, 3):
+        return "LL", "LOSE: 1-2-3 straight lose"
+    else:
+        return None
+
+
+def compare_results_pvp(player_dict):
+    wins = {k: v for k, v in player_dict.items() if player_dict[k].result == "WW"}
+    trips = {k: v for k, v in player_dict.items() if player_dict[k].result[0] == "T"}
+    points = {k: v for k, v in player_dict.items() if player_dict[k].result[0] == "P"}
+    losers = {k: v for k, v in player_dict.items() if player_dict[k].result == "LL"}
+    for tier_list in [wins, trips, points, losers]:
+        if tier_list:
+            if len(tier_list) == 1:
+                print([f"{player_dict[player].name} wins with the result '{player_dict[player].result_lf}'" for player
+                       in tier_list])
+            else:
+                # TODO: Run through levels of trips and points, then return the winner.  Else: roll-off!
+                print("Roll-off!")
+                for item in dict(sorted(tier_list.items(), key=lambda item: item[1].result, reverse=True)):
+                    print(f"{tier_list[item].name}: {tier_list[item].result_lf}")
+            break
+        else:
+            continue
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Python Cee-Lo game by Ryan Sessions
-from dice_rules import compare_results_pvp, get_roll, get_result
+from game_mechanics import compare_results_pvp, get_roll, get_result
 from player_ai import Player
 
 
@@ -7,41 +7,49 @@ def main_game():
     print('--------Cee-Lo, by Ryan Sessions--------')
     # Get player count, make sure it's from 2 to 4
     while True:
-        player_count = input("Enter the amount of total players (2-4):\n")
-        if player_count.isdigit() and (2 <= int(player_count) <= 4):
+        player_count = input("Enter the amount of total players (2-4): ")
+        player_dict = dict()
+        if player_count == '':
+            print('TEST MODE!!')
+            player_count = 4
+            for i, foo in enumerate(['Wendy', 'Ryan', 'Crystal', 'Micah']):
+                player_dict['player' + str(i + 1)] = Player(name=foo)
+            break
+        elif player_count.isdigit() and (2 <= int(player_count) <= 4):
+            player_count = int(player_count)
+            # player_count = 2  # Setting default player_count for easier prototyping
+            print(f"Total player count: {player_count}")
+
+            # Create a dictionary containing all players and collect their names
+            for i in range(player_count):
+                name = input(f"Input name for player {i + 1}: ")
+                player_dict['player_' + str(i + 1)] = Player(name=name)
             break
         print("Please enter an integer from 2 to 4.")
-    player_count = int(player_count)
-    # player_count = 2  # Setting default player_count for easier prototyping
-    print(f"Total player count: {player_count}")
 
-    # Create a dictionary containing all players and collect their names
-    player_dict = dict()
-    for i in range(player_count):
-        name = input(f"Input name for player {i + 1}:\n")
-        player_dict['player_' + str(i + 1)] = Player(name=name)
-
-    # TODO: Work out rolls, roll order, matchups, etc
+    # main game loop
     print("--------------------------------------")
     # Iterate through all players who don't have a roll result until they all do
-    while not all(player_dict[player].return_result() for player in player_dict):
+    while not all(player_dict[player].result for player in player_dict):
         for player in player_dict:
-            if not player_dict[player].return_result():
+            if not player_dict[player].result:
                 roll_list = get_roll()
                 print(f"{player_dict[player].name} rolled: {roll_list}")
                 result = get_result(roll_list)
                 if result:  # If they roll something, add the roll result to player object
-                    player_dict[player].update_result(result)
-                    print(f"{player_dict[player].name}'s result: {result}")
+                    player_dict[player].result, player_dict[player].result_lf = result[0], result[1]
+                    print(f"{player_dict[player].name}'s result: {result[1]}")
                 else:  # Notify the players about the re-roll
                     print(f"{player_dict[player].name} will need to re-roll.")
             else:
-                print(f"{player_dict[player].name}'s result: {player_dict[player].return_result()}")
+                print(f"{player_dict[player].name}'s result: {player_dict[player].result_lf}")
         print("--------------------------------------")
+    # Return results
     for player in player_dict:
-        print(f"{player_dict[player].name}'s result: {player_dict[player].return_result()}")
+        print(f"{player_dict[player].name}'s result: {player_dict[player].result_lf}")
 
     compare_results_pvp(player_dict)
+    # Do money math
 
 
 if __name__ == '__main__':
